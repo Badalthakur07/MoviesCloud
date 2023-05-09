1cac509fb17a4bc9a39a694ef4ecbde97691b8d4
Badalthakur07 <114247216+Badalthakur07@users.noreply.github.com>
2023-05-09 07:24:53 +0530

    Update res.js

--- res.js
+++ res.js
@@ -9,16 +9,6 @@ burger.addEventListener('click', ()=>{
    
 }) 
 
- document.getElementById("firstheading").addEventListener("click",change);
-
-  function change(){
-    if(document.getElementById("skill").style.visibility == "visible"){
-      document.getElementById("skill").style.visibility = "hidden";
-    }else {
-      document.getElementById("skill").style.visibility= "visible";
-    }
-  }
-
 document.addEventListener('DOMContentLoaded', () => {
   const getSearchBar = 
       document.querySelector('#searchBar');
@@ -81,21 +71,3 @@ window.addEventListener('scroll', ()=>{
  }
 
 })
- var adfly_id = 27167195;
-    var adfly_advert = 'int';
-    var popunder = true;
-    var domains = ['depositfiles.com', 'rapidshare.com', 'vip-file.com', 'smsfiles.ru', '4files.net', 'turbobit.ru', 'uploading.com', 'letitbit.net', 'depositfiles.ru', 'sms4file.com', 'ifolder.ru', 'hotfile.com', 'anyfiles.net', 'sharingmatrix.com', 'megashare.com', 'megaupload.com', 'rapidshare.de', 'rapidshare.ru', 'uploadbox.com', 'filefactory.com', 'filefactory.ru', 'filepost.ru', 'onefile.net', 'freefolder.net', 'getthebit.com', 'turbobit.net'];
-
-
-  
-    var adfly_id = 27167195;
-    var adfly_advert = 'int';
-    var frequency_cap = 5;
-    var frequency_delay = 5;
-    var init_delay = 3;
-    var popunder = true;
-
-var adfly_id = 27167195;
-    var adfly_advert = 'int';
-    var popunder = true;
-    var domains = ['depositfiles.com', 'rapidshare.com', 'vip-file.com', 'smsfiles.ru', '4files.net', 'turbobit.ru', 'uploading.com', 'letitbit.net', 'depositfiles.ru', 'sms4file.com', 'ifolder.ru', 'hotfile.com', 'anyfiles.net', 'sharingmatrix.com', 'megashare.com', 'megaupload.com', 'rapidshare.de', 'rapidshare.ru', 'uploadbox.com', 'filefactory.com', 'filefactory.ru', 'filepost.ru', 'onefile.net', 'freefolder.net', 'getthebit.com', 'turbobit.net'];
