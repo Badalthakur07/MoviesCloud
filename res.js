3f0a7dde4ecf6db12c14974facaa72e0f6ec7dff
Badalthakur07 <114247216+Badalthakur07@users.noreply.github.com>
2023-05-09 07:09:57 +0530

    Update res.js

--- res.js
+++ res.js
@@ -9,9 +9,7 @@ burger.addEventListener('click', ()=>{
    
 }) 
 
-
-
-  document.getElementById("firstheading").addEventListener("click",change);
+ document.getElementById("firstheading").addEventListener("click",change);
 
   function change(){
     if(document.getElementById("skill").style.visibility == "visible"){
@@ -21,11 +19,69 @@ burger.addEventListener('click', ()=>{
     }
   }
 
+document.addEventListener('DOMContentLoaded', () => {
+  const getSearchBar = 
+      document.querySelector('#searchBar');
+  const getAllMovies = 
+      document.querySelectorAll('.first-box');
 
+  getSearchBar.addEventListener('keyup', e => {
+      getAllMovies.forEach(movie => {
+          if (movie.innerText.toLowerCase().includes(
+              e.target.value.toLowerCase())) {
+              movie.style.display = 'block';
+              return movie;
+          }
+          else {
+              movie.style.display = 'none';
+          }
+      });
+  });
+});
   
+const container = document.querySelector('.first-section')
+    let limit = 4 ;
+    let pageCount = 1;
+    let postCount = 1;
 
+  const getUserData = async() => {
+    try {
     
-    var adfly_id = 27167195;
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=4$_page=1');
+      const data = await res.json();
+    console.log(data);
+    
+   }catch(error){
+   console.log(error);
+
+    data.map((curEln, index) => {
+      const htmlData = '<div class="first-box"> <a href="" target="_blank"> <img src="">${postCount++}</a> <p class="first-pra">${curElm.body}</p></div>';
+         
+    first-section.insertAdjacentHTML('beforeened',htmlData)
+        
+    })
+    
+}
+  }
+  getUserData();
+
+  const showData =() => {
+    setTimeout(() =>{
+      pageCount++;
+      getUserData();
+    },300)
+  }
+
+window.addEventListener('scroll', ()=>{
+ const {scrollHeight , ScrollTop, ClientHeight} = document.documentElement;
+
+ if(ScrollTop + ClientHeight >= scrollHeight){
+  console.log('I am in Button')
+  showData()
+ }
+
+})
+ var adfly_id = 27167195;
     var adfly_advert = 'int';
     var popunder = true;
     var domains = ['depositfiles.com', 'rapidshare.com', 'vip-file.com', 'smsfiles.ru', '4files.net', 'turbobit.ru', 'uploading.com', 'letitbit.net', 'depositfiles.ru', 'sms4file.com', 'ifolder.ru', 'hotfile.com', 'anyfiles.net', 'sharingmatrix.com', 'megashare.com', 'megaupload.com', 'rapidshare.de', 'rapidshare.ru', 'uploadbox.com', 'filefactory.com', 'filefactory.ru', 'filepost.ru', 'onefile.net', 'freefolder.net', 'getthebit.com', 'turbobit.net'];
